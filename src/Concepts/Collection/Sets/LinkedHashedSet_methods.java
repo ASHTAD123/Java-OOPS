befd4914c9c5da7ed47f5c04ad60aaf83e41728d
ASHTAD123 <ashtadcyrusirani@gmail.com>
2024-07-17 15:40:33 +0530

    tree set

--- src/Concepts/Collection/Sets/LinkedHashedSet_methods.java
+++ src/Concepts/Collection/Sets/LinkedHashedSet_methods.java
@@ -1,19 +1,63 @@
 package Concepts.Collection.Sets;
 
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class LinkedHashedSet_methods {
 
-	
 	public static void main(String[] args) {
 		
 		
 		LinkedHashSet lhs = new LinkedHashSet();
+		lhs.add("A");
+		lhs.add("B");
+		lhs.add("C");
+		lhs.add("D");
+		lhs.add("E");
+		lhs.add("F");
+		lhs.add("G");
+				
+		System.out.println(lhs);
 		
+		System.out.println("Size of LinkedHashSet :" +lhs.size());
 		
+		System.out.println("Removing A :" + lhs.remove("A"));
 		
+		System.out.println(lhs);
 		
+		System.out.println("Does it contain A" + lhs.contains("A"));
+		
+		System.out.println();
+		
+		System.out.println("Iterating over an LinkedHashSet ");
+		
+		Iterator i = lhs.iterator();
+		
+		while(i.hasNext()) {
+			System.out.println(i.next());
+			System.out.print(",");
+		}
+		
+		System.out.println("Adding element to Start of LinkedHashSet ");
+		lhs.addFirst("AA");
+		System.out.println(lhs);
+		
+		System.out.println("Adding element to Last of LinkedHashSet ");
+		lhs.addLast("ZZ");
+		System.out.println(lhs);
+		
+		System.out.println("Removing 1st element of LinkedHashSet");
+		lhs.removeFirst();
+		System.out.println(lhs);
+		
+		System.out.println("Removing last element of LinkedHashSet");
+		lhs.removeLast();
+		System.out.println(lhs);
+		
+		System.out.println("Reverse of this LinkedHashSet is : ");
+		lhs.reversed();
+		System.out.println(lhs);
 		
 	}
 }
---
+++ src/Concepts/Collection/Sets/TreeSet_methods.java
@@ -0,0 +1,93 @@
+package Concepts.Collection.Sets;
+
+import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class TreeSet_methods {
+	
+	public static void main(String[] args) {
+		
+		// empty tree set at beginning
+		TreeSet treeSet = new TreeSet();
+		System.out.println(treeSet);
+		
+		// public boolean add(E e)
+		treeSet.add(1);
+		treeSet.add(2);
+		treeSet.add(3);
+		treeSet.add(13);
+		
+		System.out.println(treeSet);
+		
+		// public E ceiling(E e)
+		System.out.println("Ceiling : " +treeSet.ceiling(12));
+		
+		// public E floor(E e)
+		System.out.println("Floor : " + treeSet.floor(12));
+		
+		//public boolean contains(Object o)
+		boolean res = treeSet.contains(1);
+		System.out.println(res);
+		
+		// public Iterator<E> descendingIterator()
+		System.out.println();
+		System.out.println("Iterating in Decending order using decending iterator");
+		Iterator desIterator = treeSet.descendingIterator();
+		
+		while(desIterator.hasNext()) {
+			System.out.println(desIterator.next());
+		}
+		
+        //public NavigableSet<E> descendingSet()
+		NavigableSet s = treeSet.descendingSet();
+		System.out.println("Descending Set");
+		System.out.println(s);
+		
+		
+		// public SortedSet<E> headSet(E toElement)
+		// Returns a view of the portion of this set whose elements are strictly less than toElement
+		SortedSet sortedSet = treeSet.headSet(12);
+		System.out.println(sortedSet);
+		
+		
+		// public NavigableSet<E> headSet(E toElement, boolean inclusive)
+		// Returns a view of the portion of this set whose elements
+		// are less than (or equal to, if inclusive is true) toElement.
+		NavigableSet nv = treeSet.headSet(12, true);
+		
+		
+		// first()
+		// Returns the first (lowest) element currently in this set.
+		System.out.println("Lowest in set : "+treeSet.first());
+		
+		// public E last()
+		System.out.println("Highest in set : "+treeSet.last());
+		
+		
+		// public E higher(E e)
+		// Returns the least element in this set strictly greater
+		// than the given element, or null if there is no such element.
+		System.out.println("Least element in set greater than 12 : " +treeSet.higher(12));
+		
+		
+		// public E lower(E e)
+		// Returns the greatest element in this set strictly less than the given element
+		// or null if there is no such element.
+		System.out.println("Greatest element in set less than 12 : " +treeSet.lower(12));
+		
+		
+		// public E pollFirst()
+		// Retrieves and removes the first (lowest) element, or returns null if this set is empty.
+		System.out.println("Removing the lowest elemet using poll : "+treeSet.pollFirst());
+		
+		
+		// public E pollLast()
+		// Retrieves and removes the last (highest) element, or returns null if this set is empty.
+		System.out.println("Removing the highest elemet using poll : "+treeSet.pollLast());
+		
+		System.out.println("After poll" +treeSet);
+}
+
+}
